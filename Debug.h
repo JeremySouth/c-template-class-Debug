5b2d93f7befee7d6b28f7791db89d89ba230c2f2
seungin.nam <namsikie@gmail.com>
2021-04-09 14:55:29 +0900

    added Debug.cpp
    link error fixed.

--- Debug.h
+++ Debug.h
@@ -6,6 +6,9 @@
 #include <sstream>
 #include <mutex>
 
+void OutputApiError(const char* apiName);
+void OutputApiError(const char* func, const char* apiName);
+
 class DebugLog
 {
 public:
@@ -147,24 +150,6 @@ private:
     static DebugLogFuncPtr _errFunc;
     static std::mutex _mutex;
 };
-
-decltype(DebugLog::_mode)    DebugLog::_mode = DebugLog::Mode::File;
-decltype(DebugLog::_logFunc) DebugLog::_logFunc = nullptr;
-decltype(DebugLog::_errFunc) DebugLog::_errFunc = nullptr;
-decltype(DebugLog::_fs)      DebugLog::_fs;
-decltype(DebugLog::_ss)      DebugLog::_ss;
-decltype(DebugLog::_mutex)   DebugLog::_mutex;
-
-void OutputApiError(const char* apiName)
-{
-    const auto error = ::GetLastError();
-    DebugLog::Error(apiName, "() failed with error code: ", error);
-}
-void OutputApiError(const char* func, const char* apiName)
-{
-    const auto error = ::GetLastError();
-    DebugLog::Error(func, "() => ", apiName, "() failed with error code: ", error);
-}
 
 #ifdef _DEBUG
 #define FUNCTION_SCOPE_TIMER \
